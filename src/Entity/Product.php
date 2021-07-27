10ee7777dbb13ea8727a8b3f2d530ba1d20356d8
Ilya Fertikov <rodel2690@gmail.com>
2021-07-27 14:01:30 +0400

    feat: add auction and it's relationship to product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -32,6 +34,16 @@ class Product
      * @ORM\JoinColumn(nullable=false)
      */
     private $price;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Auction::class, mappedBy="product", orphanRemoval=true)
+     */
+    private $auctions;
+
+    public function __construct()
+    {
+        $this->auctions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -70,6 +82,36 @@ class Product
     public function setPrice(Price $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Auction[]
+     */
+    public function getAuctions(): Collection
+    {
+        return $this->auctions;
+    }
+
+    public function addAuction(Auction $auction): self
+    {
+        if (!$this->auctions->contains($auction)) {
+            $this->auctions[] = $auction;
+            $auction->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAuction(Auction $auction): self
+    {
+        if ($this->auctions->removeElement($auction)) {
+            // set the owning side to null (unless already changed)
+            if ($auction->getProduct() === $this) {
+                $auction->setProduct(null);
+            }
+        }
 
         return $this;
     }
